d9a0370762a3104ae1cddd28a4e712ac646d2de2
lsk <sinrim10@gmail.com>
2016-08-25 22:23:31 +0900

    수정.

---
+++ serial_test.js
@@ -0,0 +1,25 @@
+/**
+ * Created by lsk on 2016-08-25.
+ */
+/*var SerialPort = require("serialport");
+var port = new SerialPort("/dev/tty-usbserial1", {
+    baudRate: 57600
+});*/
+
+var SerialPort = require("serialport").SerialPort;
+var serialPort = new SerialPort("/dev/ttyACM0", {
+    baudRate: 9600
+});
+
+serialPort.on("open", function () {
+    console.log('open');
+
+    serialPort.on('data', function(data) {
+        console.log('data received: ' + data);
+    });
+
+    serialPort.write(new Buffer('4','ascii'), function(err, results) {
+        console.log('err ' + err);
+        console.log('results ' + results);
+    });
+});
